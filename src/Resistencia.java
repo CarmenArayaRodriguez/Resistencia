c09c820c081847afddad53c2d6e741f398d1c737
Carmen Araya Rodríguez <carmenarayarodriguez@gmail.com>
2024-04-30 21:42:37 -0400

    Modifica codigo y diagrama para incorporar ciclo do while

--- src/Resistencia.java
+++ src/Resistencia.java
@@ -4,34 +4,43 @@ public class Resistencia {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        float R1 = 0;
+        float R2 = 0;
+        float R3 = 0;
+
         // Se solicita al usuario que ingrese 3 resistencias
-        System.out.print("Ingrese resistencia 1: ");
-        float R1 = scanner.nextFloat();
-        System.out.print("Ingrese resistencia 2: ");
-        float R2 = scanner.nextFloat();
-        System.out.print("Ingrese resistencia 3: ");
-        float R3 = scanner.nextFloat();
+        do {
+            System.out.print("Ingrese resistencia 1: ");
+            R1 = scanner.nextFloat();
+            if (R1 <= 0) {
+                System.out.println("Todas las resistencias deben ser positivas. Por favor, ingrese un valor positivo.");
+            }
+        } while (R1 <= 0);
 
-        // Se verifica que todas las resistencias ingresadas sean positivas
-        if (R1 <= 0 || R2 <= 0 || R3 <= 0) {
+        do {
+            System.out.print("Ingrese resistencia 2: ");
+            R2 = scanner.nextFloat();
+            if (R2 <= 0) {
+                System.out.println("Todas las resistencias deben ser positivas. Por favor, ingrese un valor positivo.");
+            }
+        } while (R2 <= 0);
 
-            // Si las resistencias son negativas, se muestra el mensaje:
-            System.out.println("Todas las resistencias deben ser positivas.");
-
-            // Se cierra el objeto scanner
-            scanner.close();
-
-            // Finaliza el programa
-            return;
-        }
+        do {
+            System.out.print("Ingrese resistencia 3: ");
+            R3 = scanner.nextFloat();
+            if (R3 <= 0) {
+                System.out.println("Todas las resistencias deben ser positivas. Por favor, ingrese un valor positivo.");
+            }
+        } while (R3 <= 0);
 
         // Si los valores ingresados son positivos,se calcula la resistencia total
         float RT = 1 / (1 / R1 + 1 / R2 + 1 / R3);
 
         // Se muestra el resultado con dos decimales de precisión
-        System.out.printf("La resistencia total es de %.2f\n", RT);
+        System.out.printf("La resistencia total es de %.1f\n", RT);
 
         // Se cierra el objeto scanner
         scanner.close();
     }
 }
+
